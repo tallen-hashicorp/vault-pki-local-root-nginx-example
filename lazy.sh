d605f9711ec573a7f085e4a276f676fd3e17b26f
Tyler Allen <tyler.allen@hashicorp.com>
2024-10-23 15:42:14 +0100

    adding in some readme stuff

--- lazy.sh
+++ lazy.sh
@@ -1,26 +1,5 @@
-# vault-pki-local-root-nginx-example
+#!/bin/bash
 
-## Create a Root Certificate Authority (CA)
-```bash
-openssl genrsa -out rootCA.key 4096
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.crt
-```
-
-You will be prompted for details such as the country, organization name, and Common Name (CN). For a root CA, the CN typically refers to something like "My Root CA."]
-
-**Add the `rootCA.crt` to the trust keychain to trust**
-1. Open keychain
-2. Select System Keychains > System
-3. Drag the `rootCA.crt` from finder to this
-4. Select Get Info on new cert
-5. Set to always trust
-
-Your laptop will now trust antying this signs or makes
-
-
-## Create an Intermediate CA in Vault
-This step assumes you have vault running and can access it. If you are lazy you can run `./lazy.sh`
-```bash
 # Enable the intermediate PKI secrets engine
 vault secrets enable -path=pki_int pki
 
@@ -40,11 +19,7 @@ cat intermediate-cert.pem rootCA.crt > intermediate-bundle.pem
 
 # Set the signed intermediate certificate
 vault write pki_int/intermediate/set-signed certificate=@intermediate-bundle.pem
-```
 
-## Define role and request cert for NGINX
-```bash
-# Define roles for the intermediate CA
 vault write pki_int/roles/example-dot-com \
      issuer_ref="$(vault read -field=default pki_int/config/issuers)" \
      allowed_domains="example.com" \
@@ -61,4 +36,3 @@ jq -r '.data.issuing_ca' nginx_certs.json > nginx_issuing_ca.pem
 jq -r '.data.ca_chain | join("\n")' nginx_certs.json > nginx_chain.pem
 
 openssl verify -CAfile nginx_chain.pem nginx_certificate.pem
-```
